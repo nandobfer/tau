692d5566a9ad0580aab07b52fad3f826c107a5d0
nandobfer <nandobfer@yahoo.com.br>
2023-01-23 08:23:41 -0300

    feat: added text speak to run

--- run.py
+++ run.py
@@ -1,7 +1,13 @@
 from src.speech.main import getText
 from src.utils.togglePrint import togglePrint
 from src.speech.tools.calculator import isCalculus, sum
+import pyttsx3
 import sys
+
+def speakText(command):
+    engine = pyttsx3.init()
+    engine.say(command)
+    engine.runAndWait()
 
 while True:
     togglePrint(False)
@@ -12,7 +18,8 @@ while True:
             print(text)
 
             if isCalculus(text):
-                sum(text)
+                result = sum(text)
+                speakText(result)
 
     except KeyboardInterrupt:
         sys.exit()
